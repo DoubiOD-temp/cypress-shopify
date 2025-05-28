e18719ed1d2d5105954b6a6b50e5287325d1e67f
OD <r1030640@student.thomasmore.be>
2025-05-29 00:45:21 +0200

    cypress cloud fix

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,11 @@ module.exports = defineConfig({
   projectId: "jt4q9w",
   e2e: {
     baseUrl: 'https://r1020640-realbeans.myshopify.com',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    defaultCommandTimeout: 10000,
-    requestTimeout: 10000,
-    responseTimeout: 10000,
-    pageLoadTimeout: 30000,
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: true,
     screenshotOnRunFailure: true,
-    retries: {
-      runMode: 2,
-      openMode: 0
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
   },
 });
